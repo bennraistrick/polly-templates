453b168062935047dfa5c7309dfbce12fac8f260
Benn Raistrick <benn.raistrick@gmail.com>
2014-04-12 00:36:15 +0100

    Less structure for Avant theme
    building out molecules, properties, elements, to multi/single cells.
    initial 1 version of each multi-cell as a starting point.

---
+++ avant/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta>
+		<meta>
+		<title>The Grid _ Avant Theme</title>
+
+		<!-- Bootstrap -->
+		<link rel="stylesheet/less" type="text/css" href="css/style.less">
+
+		<!-- Typeface -->
+		<script type="text/javascript" src="//use.typekit.net/hpv8izz.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+		<!-- LESS -->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.0/less.min.js"></script>
+
+	</head>
+	<body>
+
+		<!-- header-1 -->
+		<section class="header-1">
+			<div class="title-1">
+				<h1>This is the hero of our theme</h1>
+				<h2>and this is a slide title</h2>
+			</div>
+		</section>
+		<!-- end header-1 -->
+		
+		<!-- multi-cell unit-1 -->
+		<section class="unit-1 container-fluid">
+			<article>
+				
+				<!-- cell intro-1 -->	
+				<div class="insert-1">
+
+					<!-- component title-1 -->
+					<div class="col-md-8 col-md-offset-2">
+						<h2>Nascetur augue scelerisque cras magna porta</h2>
+						<p>Arcu, sed mus tristique magnis et, a sit sed tristique amet quis! Lundium! Quis! Duis montes sit velit. Dignissim? Phasellus tortor in dis a! Lectus nunc pulvinar, enim. Lectus urna, dolor porta, porttitor, in? Natoque. Ridiculus sociis scelerisque ut est porta ut, scelerisque nascetur facilisis.</p>
+					</div>
+
+					<!-- component list-1 -->
+					<div class="col-md-12">
+						<hr>
+						<div class="col-md-3">
+							<h4>Lectus nunc pulvinar</h4>
+							<p>Lectus urna, dolor porta, porttitor, in? Natoque. Ridiculus sociis scelerisque ut est porta ut, scelerisque nascetur facilisis.</p>
+						</div>
+
+						<div class="col-md-3">
+							<h4>Lectus nunc pulvinar</h4>
+							<p>Lectus urna, dolor porta, porttitor, in? Natoque. Ridiculus sociis scelerisque ut est porta ut, scelerisque nascetur facilisis.</p>
+						</div>
+
+						<div class="col-md-3">
+							<h4>Lectus nunc pulvinar</h4>
+							<p>Lectus urna, dolor porta, porttitor, in? Natoque. Ridiculus sociis scelerisque ut est porta ut, scelerisque nascetur facilisis.</p>
+						</div>
+
+					</div>
+				</div>
+					
+				
+			</article>
+		</section>
+		<!-- end unit-1 -->
+		
+		<footer>
+		</footer>
+		
+		<script type="text/javascript">
+			less.env = 'development';
+			less.watch();
+		</script>
+	</body>
+</html>
